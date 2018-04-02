91bf1c59db60321304095786b23ba3e9e207dc3e
vitor-lv <37788429+vitor-lv@users.noreply.github.com>
2018-04-02 15:36:49 -0300

    Update cartao.js

--- js/cartao.js
+++ js/cartao.js
@@ -2,62 +2,6 @@
 
 	const cartoes = document.querySelectorAll(".cartao")
 
-	// for (let j = 0; j < cartoes.length; j++) {
-	// 	const cartao = cartoes[j]
-
-	// 	cartao.on("focusin", function (){
-	// 		cartao.classList.add("cartao--focado")
-	// 	})
-
-	// 	cartao.on("focusout", function (){
-	// 		cartao.classList.remove("cartao--focado")
-	// 	})
-	
-
-	//  	cartao.on("change", ".opcoesDoCartao-radioTipo", function mudaCor (event) {
-	//  		cartao.css("background-color", event.target.value)
-
-	//  			// if(cartao.style.backgroundColor == 'rgb(240, 84, 80)'){
-	// 				// cartao.classList.add("teste")
-
-	//  			// } else(cartao.classList.remove("teste"))
-	//  	})
-
-
-	 			
-
-
-
-
- 			
- // 		cartao.addEventListener('click', function() {
-	// 	if(event.target.classList.contains("opcoesDoCartao-remove")){
-			
-	// 		cartao.addEventListener("transitionend", function (event) {
-	// 		if (event.propertyName == 'opacity') {
-	// 		cartao.remove()
-	// 		}
-	// 		})
-
-	// 		cartao.classList.add("cartao--some")
-		
-	// 		}
-	// 	})
-
-	// 	cartao.addEventListener("keyup", function(event){
-	// 		const isLabelTipo = event.target.classList.contains("opcoesDoCartao-tipo")
-			
-	// 		if(isLabelTipo && (event.key === "Enter" || event.key === " ")) {
-	// 			event.target.click()
-	// 		}
-	// 	})
-	// }
-
-
-	
-
-
-
 	let numeroDoCartao = 0
 	const form = document.querySelector(".formNovoCartao")
 
