0ad88de1787fa2b3dec816262ad52b83db1ecd5a
vitor-lv <37788429+vitor-lv@users.noreply.github.com>
2018-04-02 15:35:03 -0300

    Update btnAjuda.js

--- js/btnAjuda.js
+++ js/btnAjuda.js
@@ -1,29 +1,3 @@
-// ;(function() {
-
-    	
-// 	const btnAjuda = document.querySelector("#btnAjuda")
-	
-	
-	
-
-// 	btnAjuda.addEventListener("click", function(){
-        
-//     const ajudas = [{conteudo: "Bem-vindo ao Ceep", cor: "#f05450"},
-//     {conteudo: "Clique no botão linhas para alterar o Layout", cor: "#92c4ec"}]
-
-// 		ajudas.forEach(function(ajuda){
-            
-//         window.ceep.addCartaoMural(ajuda)
-
-//         })
-//     })
-    
-//     btnAjuda.classList.remove('no-js')
-
-// })()
-        
-
-
 ;(function() {
         
                 
